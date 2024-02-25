247cfc7c7337e0bf75d713016acdf6d086289b16
Sinth <sinthmite@gmail.com>
2024-02-24 19:20:34 -0500

    Functionality of site, i believe is complete

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const submit = document.getElementById('submit');
 const form = document.getElementById('form');
 const main = document.querySelector('main');
 const popUp = document.getElementById('popUp');
+const confirmation = document.getElementById('confirmation');
+const dismiss = document.getElementById('dismiss');
 
 function newPage() {
     main.style.display = "none";
@@ -13,7 +15,22 @@ function fail(){
     errorMessage.hidden = false;
     emailInput.style.borderColor = "hsl(4, 100%, 67%)"
 }
+function reset(){
+    main.style.display = "";
+    popUp.style.display = "none";
+    emailInput.value = ""
+}
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    newPage()
+    const emailPattern = /^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$/i;
+
+    if (!emailPattern.test(emailInput.value)) {
+        fail();
+    } else {
+        newPage();
+        confirmation.textContent = `A confirmation email has been sent to ${emailInput.value}. Please open it and click the button inside to confirm your subscription.`;
+        emailInput.style.fontWeight = "800";
+    }
 })
+
+dismiss.addEventListener('click', reset);
